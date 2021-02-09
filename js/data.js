925ae0324c9d1d1507e2b1a610b73aaa0a15d9b7
Osama Khan <osamaa.khan61@gmail.com>
2021-02-08 17:32:48 -0800

    Listen for 'submit' events on the journal entry form #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,11 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var $submitButton = document.querySelector('button');
+
+$submitButton.addEventListener('submit', function (event) {
+  var formJSON = JSON.stringify(data);
+  localStorage.setItem('form-local-storage', formJSON);
+})
+;
